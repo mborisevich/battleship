52bca48b6384d05b9ac2a5471e184017e59afc97
mborisevich <maxborisevich@home.nl>
2025-03-05 21:34:38 +0100

    Expand gameboard function to allow placement of ships & collision detection

--- all.test.js
+++ all.test.js
@@ -1,7 +1,7 @@
 import {Ship, Coordinate, Gameboard} from "./main.js"
-const testShip = new Ship(3)
 const testGameboard = new Gameboard()
-testGameboard.placeShip(testShip, 3, 0, "up")
+const testShip = testGameboard.createBattleship()
+testGameboard.placeShip(testShip, 0, 3, "right")
 test("length is larger than 0", ()=> {
     expect(testShip.length).toBeGreaterThanOrEqual(0);
 }
@@ -11,11 +11,6 @@ test("length is smaller than 4", ()=> {
 }
 )
 
-testShip.hit()
-
-test("expect health to be 2 after 1 hit", () => {
-    expect(testShip.health).toBe(2);
-})
 
 test("The gameboard contains 10 columns with Coordinate objects)", () => {
     expect(testGameboard.grid.length).toBe(10)
@@ -25,15 +20,18 @@ test("Each column contains 10 rows with with Coordinate objects)", () => {
 })
 
 test("The gameboard contains a ship at x = 0 and y = 3", () => {
-    expect(testGameboard.grid[3][0].contains).toBe(testShip)
+    expect(testGameboard.containsShip(0,3)).toBe(true)
 })
 test("The gameboard contains a ship at x = 1 and y = 3", () => {
-    expect(testGameboard.grid[3][1].contains).toBe(testShip)
+    expect(testGameboard.containsShip(1,3)).toBe(true)
 })
 test("The gameboard contains a ship at x = 2 and y = 3", () => {
-    expect(testGameboard.grid[3][2].contains).toBe(testShip)
+    expect(testGameboard.containsShip(2,3)).toBe(true)
 })
-
-test("return missed shot coordinates", () => {
-    expect(testGameboard.receiveAttack(5,7).toBe([5,7]))
+test("containsShip finds ship in correct grid", () => {
+    expect(testGameboard.containsShip(0,3)).toBe(true)
+})
+test("Ship loses 1 health after ReceiveAttack is called on coordinates", () => {
+    testGameboard.receiveAttack(0,3);
+    expect(testShip.health).toBe(2)
 })
--- main.js
+++ main.js
@@ -1,19 +1,25 @@
-export {Ship, Coordinate, Gameboard}
+//export {Ship, Coordinate, Gameboard}
 class Ship {
-    constructor(length){
+    constructor(name, length){
+        this.name = name
         this.length = length;
         this.health = length;
         this.sunk = false;
+        this.position = []
         this.hitHull = [];
     }
     hit(){
         this.health = this.health - 1
         this.#checkSunk()
     }
+    giveName(){
+        return this.name
+    }
 
     #checkSunk(){
         if (this.health < 0){
             this.sunk = true
+            console.log("Ship has sunk!")
         }
     }
 }
@@ -21,11 +27,15 @@ class Coordinate {
     constructor(){
         this.contains = 0
     }
+    printCoordinate(){
+        console.log(contains)
+    }
 }
 class Gameboard {
     constructor(){
         this.grid = this.initializeBoard()
-        this.missedShots = []
+        this.shots = []
+        this.hits = []
         this.totalShips = 6
     }
 
@@ -40,24 +50,107 @@ class Gameboard {
         }
         return emptyGrid
     }
-
+   
     //Add ships to the board
+
+    containsShip(x,y){
+        if (this.grid[y][x].contains instanceof Ship){
+            return true
+        } else {
+            return false
+        }
+    }
+    getShip(x,y){
+        return this.grid[y][x].contains
+    }
+    printBoard(){
+        for (let i = 0; i < 10; i++){
+            let displayedGrid = this.grid[i].map((val) => {
+                if (val.contains instanceof Ship){
+                    return "s"
+                } else {
+                    return "#"
+                }
+            })
+            console.log(displayedGrid.toString())
+        }
+    }
+    
+    executeAttack(x,y){
+        if (this.containsShip(x,y)){
+            this.getShip(x,y).hit()
+            this.hits.push([x,y])
+            return console.log(`Ship hit at coordinates: x=${x},y=${y}`)
+        }
+        else {
+            this.missedShots.push([x,y])
+            return console.log("No hit!")
+        }
+    }
+    receiveAttack(x,y){
+        if (x > 10 || y > 10){
+            console.log("Coordinates out of bounds!")
+        } else if (this.hits.includes([x,y]) || this.shots.includes([x,y])){
+            console.log("This grid has already been targeted")
+        }
+        else {
+            return this.executeAttack(x,y)
+        }
+    }
+    checkCollisions(ship, x, y , direction){
+        let coordinateOccupied = false
+        if (direction == "left"){
+            for (let i = 0; i < ship.length; i++){
+                if (this.containsShip(x-i,y)){
+                    coordinateOccupied = true
+                }
+            }
+        } else if (direction == "right"){
+            for (let i = 0; i < ship.length; i++){
+                if (this.containsShip(x+i,y)){
+                    coordinateOccupied = true
+                }
+            }
+        } else if (direction == "up") {
+            for (let i = 0; i < ship.length; i++){
+                if (this.containsShip(x,y+i)){
+                    coordinateOccupied = true
+                }
+            }
+        } else if (direction == "down"){
+            for (let i = 0; i < ship.length; i++){
+                if (this.containsShip(x-i,y)){
+                    coordinateOccupied = true
+                }
+            }
+        }
+        return coordinateOccupied
+    }
+    createBattleship(){
+        return new Ship("Battleship", 3)
+    }
+    createCruiser(){
+        return new Ship("Cruiser", 2)
+    }
+    createDestroyer(){
+        return new Ship("Destroyer", 1)
+    }
     placeShip(ship, x, y, direction){
-        if (direction == "left" && (x - ship.length >= 0)){
+        if (direction == "left" && (x - ship.length >= 0) && !this.checkCollisions(ship,x,y,direction)){
             for (let i = 0; i < ship.length; i++){
-                this.grid[x-i][y].contains = ship
+                this.grid[y][x-i].contains = ship
             }
-        } else if (direction == "right" && (x+ ship.length <= 10)){
+        } else if (direction == "right" && (x+ ship.length <= 10) && !this.checkCollisions(ship,x,y,direction)){
             for (let i = 0; i < ship.length; i++){
-                this.grid[x+i][y].contains = ship
+                this.grid[y][x+i].contains = ship
             }
-        } else if (direction == "up" && (y + ship.length <= 10)){
+        } else if (direction == "up" && (y + ship.length <= 10) && !this.checkCollisions(ship,x,y,direction)){
             for (let i = 0; i < ship.length; i++){
-                this.grid[x][y+i].contains = ship
+                this.grid[y+i][x].contains = ship
             }
-        } else if (direction == "down" && (y - ship.length >= 0)) {
+        } else if (direction == "down" && (y - ship.length >= 0) && !this.checkCollisions(ship,x,y,direction)) {
             for (let i = 0; i < ship.length; i++){
-                this.grid[x][y+i].contains = ship
+                this.grid[y-i][x].contains = ship
             }
         } else {
             console.log("Ship cannot be placed this way!")
@@ -65,3 +158,4 @@ class Gameboard {
     }
 
 }
+
